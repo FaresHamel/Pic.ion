d5f3bc833e7b4b0448f5aa5a1f512dd3f453ab74
algerieno <aymen.algerieno2014@gmail.com>
2020-08-27 10:52:02 +0100

    foldering before routing

---
+++ src/login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './login.css';
+
+
+function Login() {
+  return (
+    <h1>Login Page</h1>
+  );
+}
+
+export default Login;
---
+++ src/sign-up/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './signup.css';
+
+
+function Signup() {
+  return (
+    <h1>Sign up Page</h1>
+  );
+}
+
+export default Signup;
