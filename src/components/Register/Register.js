06cf14b2687444c7c5313e4924d5bd001ad14230
algerieno <aymen.algerieno2014@gmail.com>
2020-08-30 11:27:46 +0100

    fcked

---
+++ src/components/Register/Register.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Register.css';
+
+const Register = () => (
+  <div className="signup">
+    signup Component
+  </div>
+);
+
+
+
+export default Register;
