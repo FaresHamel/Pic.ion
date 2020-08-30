7813998a65c415e7688ddc9fe6a3101908f88a53
algerieno <aymen.algerieno2014@gmail.com>
2020-08-30 12:41:30 +0100

    login register alignments fixed

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,26 +4,26 @@ import './login.css';
 const login = () => (
   <div className="login">
     <div className="container">
-      <div className="card col-lg-7">
+      <div className="card col-lg-7 mx-auto">
         <div className="card-body">
           <h3 className="card-title text-center">Welcome back</h3>
           <p className="card-text text-center">use your credentials to get to your account and discover more.</p>
 
           <form>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
-            <div class="form-group">
-              <label for="exampleInputPassword1">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
             </div>
-            <div class="form-group form-check">
-              <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-              <label class="form-check-label" for="exampleCheck1">Remember me</label>
+            <div className="form-group form-check">
+              <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+              <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
             </div>
-            <button type="submit" class="btn btn-primary">Hop in</button>
+            <button type="submit" className="btn btn-primary">Hop in</button>
           </form>
 
         </div>
